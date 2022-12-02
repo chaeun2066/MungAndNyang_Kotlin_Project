76f32f988f1d7a1f0472060a27aae19e8998fccd
h_hxii <notice0601@gmail.com>
2022-12-02 14:40:09 +0900

    [CREATE] hjm_feature ReviewDetailActivity.kt and activity_review_detail Files Create

---
+++ app/src/main/java/com/example/mungandnyang/ReviewDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mungandnyang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_detail)
+    }
+}
