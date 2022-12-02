c27b849be21f818817058ca2e84cce2c183f6fb3
Chae-eun Kim <chaeun2066@gmail.com>
2022-12-02 15:48:15 +0900

    [UPDATE] kce_feature OpenApi 2 Files

--- app/src/main/java/com/example/mungandnyang/AdoptOpenApi.kt
+++ app/src/main/java/com/example/mungandnyang/AdoptOpenApi.kt
@@ -1,4 +1,19 @@
 package com.example.mungandnyang
 
+import com.example.mungandnyang.animalData.AdoptAnimal
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 class AdoptOpenApi {
+    companion object{
+        const val DOMAIN = "http://openapi.seoul.go.kr:8088"
+        const val API_KEY = BuildConfig.ADOPT_KEY
+        const val LIMIT = 32
+    }
+}
+
+interface AdoptOpenService{
+    @GET("{api_key}/json/TbAdpWaitAnimalView/1/{end}")
+    fun getAdopt(@Path("api_key") key : String, @Path("end") limit : Int): Call<AdoptAnimal>
 }
