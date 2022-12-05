2dade74ae15f4e0b40834c842b51a877ff3d893e
h_hxii <notice0601@gmail.com>
2022-12-05 10:45:56 +0900

    [UPDATE] hjm_feature ReviewAdapter.kt File Update

--- app/src/main/java/com/example/mungandnyang/ReviewAdapter.kt
+++ app/src/main/java/com/example/mungandnyang/ReviewAdapter.kt
@@ -2,6 +2,7 @@ package com.example.mungandnyang
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -17,43 +18,27 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
-class ReviewAdapter(val context: Context, val animalList: MutableList<AnimalVO>, val photoList: MutableList<PhotoVO>): RecyclerView.Adapter<ReviewAdapter.WifiViewHolder>() {
-    lateinit var databaseReference: DatabaseReference
+class ReviewAdapter(val context: Context, val reviewList: MutableList<ReviewVO>): RecyclerView.Adapter<ReviewAdapter.WifiViewHolder>() {
     var address: String? = null
 
     override fun onBindViewHolder(wifiViewholder: WifiViewHolder, position: Int) {
         val binding = (wifiViewholder as WifiViewHolder).binding
-        databaseReference = Firebase.database.reference
 
-        val animalData = animalList.get(position)
+        val reviewData = reviewList.get(position)
 
-        databaseReference.child("ReviewAdapter").addValueEventListener(object: ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                for(dataSnapshot in snapshot.children){
-                    val animalPhoto = dataSnapshot.getValue(PhotoVO::class.java)
-                    if(animalPhoto!!.ani_number.equals(animalData.number)){
-                        Glide.with(context).load("https://${animalPhoto.photo_url}").into(binding.ivRevPicture)
-                        address = "http://${animalPhoto.photo_url}"
-                    }
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("com.example.database", "onCancelled")
-            }
-        })
-
-        binding.tvRevName.text = animalData.name
+        binding.tvRevTitle.text = reviewData.title
+        binding.tvRevDate.text = reviewData.date
+        Glide.with(context)
+            .load("https://cafe.naver.com/common/storyphoto/viewer.html?src=https%3A%2F%2Fcafeptthumb-phinf.pstatic.net%2FMjAyMjA5MDhfMjc0%2FMDAxNjYyNjE0NzIxNTY2.0tjbLn317ChbfqJ-oY2AazlnOSVLU7Kds8TE3M48rF8g.qLnn1UHJ8oyG0lpc-zmI3j3RVHlR3oA4nPTXBUAuFmog.JPEG%2F20220820%25EF%25BC%25BF101630.jpg%3Ftype%3Dw1600")
+            .error(R.drawable.hashtag)
+            .fallback(R.drawable.pet_icon)
+            .into(binding.ivRevPicture)
+        Log.d("mungandnyang", "${reviewData.image}")
 
         binding.revLinearLayout.setOnClickListener {
-            val intent = Intent(binding.root.context, DetailInfoActivity::class.java)
-            intent.putExtra("imageUrl", address)
-            Log.d("com.example.database", "${address}")
-            intent.putExtra("no", animalData.number)
-            intent.putExtra("name", animalData.name)
-            intent.putExtra("gender", animalData.gender)
-            intent.putExtra("age", animalData.age)
-            intent.putExtra("kind", animalData.breed)
-            intent.putExtra("date", animalData.date)
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.data = Uri.parse(reviewData.url)
             startActivity(binding.root.context, intent, null)
         }
     }
@@ -63,7 +48,7 @@ class ReviewAdapter(val context: Context, val animalList: MutableList<AnimalVO>,
         return WifiViewHolder(binding)
     }
 
-    override fun getItemCount() = animalList.size
+    override fun getItemCount() = reviewList.size
 
     class WifiViewHolder(val binding: ReviewItemBinding) : RecyclerView.ViewHolder(binding.root)
 }
