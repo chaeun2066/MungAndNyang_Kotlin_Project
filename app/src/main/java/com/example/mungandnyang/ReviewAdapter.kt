b3ddc6f7e892ce0702fc9c751cba020625499064
h_hxii <notice0601@gmail.com>
2022-12-05 13:36:06 +0900

    [UPDATE] hjm_feature ReviewAdapter.kt File Update

--- app/src/main/java/com/example/mungandnyang/ReviewAdapter.kt
+++ app/src/main/java/com/example/mungandnyang/ReviewAdapter.kt
@@ -28,13 +28,11 @@ class ReviewAdapter(val context: Context, val reviewList: MutableList<ReviewVO>)
 
         binding.tvRevTitle.text = reviewData.title
         binding.tvRevDate.text = reviewData.date
-        Glide.with(context)
-            .load("https://cafe.naver.com/common/storyphoto/viewer.html?src=https%3A%2F%2Fcafeptthumb-phinf.pstatic.net%2FMjAyMjA5MDhfMjc0%2FMDAxNjYyNjE0NzIxNTY2.0tjbLn317ChbfqJ-oY2AazlnOSVLU7Kds8TE3M48rF8g.qLnn1UHJ8oyG0lpc-zmI3j3RVHlR3oA4nPTXBUAuFmog.JPEG%2F20220820%25EF%25BC%25BF101630.jpg%3Ftype%3Dw1600")
-            .centerCrop()
-            .into(binding.ivRevPicture)
+        binding.ivRevPicture.setImageResource(reviewData.image)
         Log.d("mungandnyang", "${reviewData.image}")
 
         binding.revLinearLayout.setOnClickListener {
+            binding.revLinearLayout.resources.getColor(R.color.iphone_blue)
             val intent = Intent()
             intent.action = Intent.ACTION_VIEW
             intent.data = Uri.parse(reviewData.url)
