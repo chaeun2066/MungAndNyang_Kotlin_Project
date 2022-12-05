739c86c18fb58667598a877fa8e3bc56037a3bf7
h_hxii <notice0601@gmail.com>
2022-12-05 13:36:38 +0900

    [UPDATE] hjm_feature ReviewUploadActivity.kt File Update

--- app/src/main/java/com/example/mungandnyang/ReviewUploadActivity.kt
+++ app/src/main/java/com/example/mungandnyang/ReviewUploadActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.example.mungandnyang.databinding.ActivityReviewUploadBinding
@@ -34,6 +35,25 @@ class ReviewUploadActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
             requestLauncher.launch(intent)
+        }
+
+        binding.ivRevupGender.setOnClickListener {
+
+        }
+
+        binding.btnRevupReg.setOnClickListener {
+            if(binding.ivRevupPicture.drawable != null && binding.tvRevupDate.text.isNotEmpty()){
+                val adoptDAO = AdoptDAO()
+                val name = binding.tvRevupName.text.toString()
+                val breed = binding.tvRevupKind.text.toString()
+                val date = binding.tvRevupDate.text.toString()
+                val text = binding.tvRevupText.text.toString()
+                val uploadData = UploadVO(name, breed, date, text)
+            }else{
+                Toast.makeText(this, "사진과 날짜를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
