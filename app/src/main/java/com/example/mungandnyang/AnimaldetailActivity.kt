7a43524ae046a2782f82c8860ac394808e611eef
Chae-eun Kim <chaeun2066@gmail.com>
2022-12-05 09:31:09 +0900

    [CREATE] kce_feature AnimaldetailActivity.kt / activity_animaldetail.xml

---
+++ app/src/main/java/com/example/mungandnyang/AnimaldetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mungandnyang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimaldetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animaldetail)
+    }
+}
