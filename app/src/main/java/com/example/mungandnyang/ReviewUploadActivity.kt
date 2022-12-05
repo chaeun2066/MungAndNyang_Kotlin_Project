6e728e28af4042b3f1f3bda79300e35de36180c7
h_hxii <notice0601@gmail.com>
2022-12-05 11:24:45 +0900

    [UPDATE] hjm_feature ReviewUploadActivity.kt File Update

--- app/src/main/java/com/example/mungandnyang/ReviewUploadActivity.kt
+++ app/src/main/java/com/example/mungandnyang/ReviewUploadActivity.kt
@@ -1,18 +1,39 @@
 package com.example.mungandnyang
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.mungandnyang.databinding.ActivityReviewUploadBinding
 
 class ReviewUploadActivity : AppCompatActivity() {
     lateinit var binding: ActivityReviewUploadBinding
+    var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.ivRevupPicture.setImageURI(intent.getStringExtra("uploadImage") as Uri)
+        val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == Activity.RESULT_OK){
+                Glide.with(applicationContext).load(it.data?.data).centerCrop().into(binding.ivRevupPicture)
+                imageUri = it.data?.data
+            }
+        }
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        requestLauncher.launch(intent)
+
+        binding.ivRevupPicture.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            requestLauncher.launch(intent)
+        }
     }
 }
